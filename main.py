9071dbd0e9eee9fb3b398f9179ab9d62d3d9dd07
Cameron Morgan <hello@cameron-morgan.com>
2023-12-09 14:49:08 +1000

    Refactor!

--- main.py
+++ main.py
@@ -1,96 +1,114 @@
-import utime, random
+import utime, time, random
 from machine import Pin
 from picodfplayer import DFPlayer
 
-clock_led = Pin(13, Pin.OUT)
-alarm_trigger = Pin(4, Pin.IN, Pin.PULL_UP)
-mode_on = Pin(21, Pin.IN, Pin.PULL_UP)
-device_on = Pin(20, Pin.IN, Pin.PULL_UP)
-
 # DFPlayer
-UART_INSTANCE=0
+UART_INSTANCE = 0
 TX_PIN = 16
-RX_PIN=17
-BUSY_PIN=22
+RX_PIN = 17
+BUSY_PIN = 22
 
-player=DFPlayer(UART_INSTANCE, TX_PIN, RX_PIN, BUSY_PIN)
+# Between 0-30
+DEFAULT_VOLUME = 18
 
-# Volume Control
-POTENTIOMETER_PIN = 26
+player = DFPlayer(UART_INSTANCE, TX_PIN, RX_PIN, BUSY_PIN)
+
+# Clock I/O
+CLOCK_LED = Pin(13, Pin.OUT)
+ALARM = Pin(12, Pin.IN, Pin.PULL_UP)
+MODE_1= Pin(21, Pin.IN, Pin.PULL_UP)
+MODE_2= Pin(20, Pin.IN, Pin.PULL_UP)
 
 # Stepper Motor
-STEPS_PER_MINUTE = 25
-
 IN1 = Pin(9, Pin.OUT)
 IN2 = Pin(7, Pin.OUT)
 IN3 = Pin(8, Pin.OUT)
 IN4 = Pin(6, Pin.OUT)
 
-pins = [IN1, IN2, IN3, IN4]
+# Adjust to match the Copal mechanism
+STEPS_PER_MINUTE = 25
 
+pins = [IN1, IN2, IN3, IN4]
 sequence = [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
 
-# Read the pot for volume control
-def read_potentiometer_value(pin):
-    return pin.read_u16()
-
-# Ordinary Operation
-def music_box():
-  if mode_on.value() != 1:
-    player.setPlaybackMode(1)
-    clock_led.value(1)
-    
-  if alarm_trigger.value() != 1:
-    alarm()
-
-# Trigger a specific song when alarm is triggered.
-def alarm():
-  clock_led.value(1)    
-
-  if random.randint(1, 10) == 10 and device_on.value() != 1:
-
-    # Play Rick Astley's infamous single, 'Never Going to Give You Up'
-    player.playTrack(2,1)
-    print("I'm never going to give [up this joke].")
-
-  elif mode_on.value() != 1 or device_on.value() != 1:
-
-    # Play Sonny & Cher 'I Got You Babe' 
-    player.playTrack(2,2)
-    print("Urgh, Groundhog Day. Another Day.")
-
-  else:
-    print("Exited. Alarm triggered when clock off.")
-
-while True:
-  # Fetch current time
-  current_time = utime.localtime()
+def rickroll():
+  player.playTrack(2,1)
   
+  while player.queryBusy() == True:
+    time.sleep(0.5)
+    CLOCK_LED.value(1)
+    time.sleep(0.5)
+    CLOCK_LED.value(0)
+
+  # Once song has finished, it should resume to main loop.
+
+def groundhog():
+  player.playTrack(1,1)
+
+  while player.queryBusy() == True:
+    CLOCK_LED.value(1)
+    
+  # Once song has finished, it should resume to main loop.
+
+def alarm():
+
+  # Pick a number between 1 and 10 to decide what alarm.
+  if random.randint(1, 10) == 10:
+    print("I'm never going to give [up this joke].")
+    rickroll()
+  else:
+    print("Urgh, Groundhog Day. Another Day.")
+    groundhog()
+
+  # Turn the LED off and return to main loop.
+  CLOCK_LED.value(0)
+
+def clock():
+  current_time = utime.localtime()
+
+  # Advance the stepper motor if the time has advanced (as it should?)
   if current_time[5] == 0:
 
-    # Advance the stepper motor
     for _ in range(STEPS_PER_MINUTE):
       for step in sequence:
         for i in range(len(pins)):
           pins[i].value(step[i])
           utime.sleep(0.001)
+    
+    print("Moved the clocks forward, Master.")
 
-  # Read the analog value from the potentiometer
-  potentiometer_value = read_potentiometer_value(machine.ADC(POTENTIOMETER_PIN))
+def init():
 
-  # Map the potentiometer value to the volume range of DFPlayer Mini (0-30)
-  volume = int(30 * potentiometer_value / 65535)
+  # Make sure LED is off before we do anything else.
+  CLOCK_LED.value(0)
+  
+  # Set a default volume. 30 is VERY LOUD with this radio's tiny speaker.
+  player.setVolume(DEFAULT_VOLUME)
+  
+  mainLoop()
 
-  # Set the volume of DFPlayer Mini
-  player.setVolume(volume)
+#
+# To help ole' mate. We're going to use 01 for music, 02 for the alarm.
+# I've wired a USB plug to a TRRS jack so he can change the music as he wishes.
+#
+# I use init() to reset the clock back. mainLoop is where the magic happens.
+#
+#
 
-  # Consider device on if set to 'On' or 'Manual'
-  if device_on.value() != 1 or mode_on.value() != 1:
-    music_box()
+def mainLoop():
+  while True:
+    clock()
 
-  # Else, consider the clock off.
-  else:
-    player.pause()
-    clock_led.value(0)
+    if MODE_1.value() != 1:
+      # Shuffle songs on flash drive
+      player.setPlaybackMode(3)
+    else:
+      player.pause()
+      
+    if ALARM.value() != 1:
+      alarm()
+    
+    # Save some CPU cycles.
+    time.sleep(1)
 
-  utime.sleep_ms(100)
+init()
